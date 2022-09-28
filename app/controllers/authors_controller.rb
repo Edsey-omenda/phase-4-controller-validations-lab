28cbff19a48a4f326f5ff126d2c15822e15c760d
Edsey omenda <omendajames0317@gmail.com>
2022-09-28 15:16:34 +0300

    completed

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,6 @@
 class AuthorsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   
   def show
     author = Author.find(params[:id])
@@ -7,9 +9,8 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
-
-    render json: author, status: :created
+    author = Author.create!(author_params)
+      render json: author, status: :created
   end
 
   private
@@ -18,4 +19,10 @@ class AuthorsController < ApplicationController
     params.permit(:email, :name)
   end
   
+  private
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
+
 end
